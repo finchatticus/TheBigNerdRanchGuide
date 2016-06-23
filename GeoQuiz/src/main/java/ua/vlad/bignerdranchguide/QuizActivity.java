5a38e645920b795d81fd20ae702f65c3d06099a2
vlad <kozii.vladyslav@gmail.com>
2016-06-23 00:24:33 +0000

    Create MVC pattern in GeoQuiz app

--- GeoQuiz/src/main/java/ua/vlad/bignerdranchguide/QuizActivity.java
+++ GeoQuiz/src/main/java/ua/vlad/bignerdranchguide/QuizActivity.java
@@ -5,33 +5,76 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button buttonTrue;
     private Button buttonFalse;
+    private Button buttonNext;
+    private TextView textViewQuestion;
+
+    private Question[] questionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    private int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        buttonTrue = (Button) findViewById(R.id.buttonTrue);
-        buttonFalse = (Button) findViewById(R.id.buttonFalse);
+        textViewQuestion = (TextView) findViewById(R.id.text_view_question);
+        updateQuestion();
 
+        buttonTrue = (Button) findViewById(R.id.button_true);
         buttonTrue.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.toast_correct, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
+        buttonFalse = (Button) findViewById(R.id.button_false);
         buttonFalse.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.toast_incorrect, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        buttonNext = (Button) findViewById(R.id.button_next);
+        buttonNext.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentIndex = (currentIndex + 1) % questionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = questionBank[currentIndex].getTextResId();
+        textViewQuestion.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = questionBank[currentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue) {
+            messageResId = R.string.toast_correct;
+        } else {
+            messageResId = R.string.toast_incorrect;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
