e414ce0a229fd20b688870ab14ed99b98aa46372
vlad <kozii.vladyslav@gmail.com>
2016-07-21 23:07:51 +0000

    GeoQuiz: Add loging inQuizActivity

--- GeoQuiz/src/main/java/ua/vlad/bignerdranchguide/QuizActivity.java
+++ GeoQuiz/src/main/java/ua/vlad/bignerdranchguide/QuizActivity.java
@@ -2,6 +2,7 @@ package ua.vlad.bignerdranchguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = QuizActivity.class.getSimpleName();
 
     private Button buttonTrue;
     private Button buttonFalse;
@@ -30,6 +33,8 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate called");
+
         setContentView(R.layout.activity_quiz);
 
         textViewQuestion = (TextView) findViewById(R.id.text_view_question);
@@ -98,5 +103,35 @@ public class QuizActivity extends AppCompatActivity {
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
     }
 }
