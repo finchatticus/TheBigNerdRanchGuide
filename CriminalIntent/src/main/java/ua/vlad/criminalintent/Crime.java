acb4b09ddc669212cbe03649fe90224ab3961225
vlad <kozii.vladyslav@gmail.com>
2016-08-04 14:12:56 +0000

    CriminalIntent: Basic version

---
+++ CriminalIntent/src/main/java/ua/vlad/criminalintent/Crime.java
@@ -0,0 +1,28 @@
+package ua.vlad.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+    private UUID id;
+    private String title;
+
+    public Crime() {
+        id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ CriminalIntent/src/main/java/ua/vlad/criminalintent/CrimeFragment.java
@@ -0,0 +1,49 @@
+package ua.vlad.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+    private Crime crime;
+
+    private EditText editTextTitle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        crime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        editTextTitle = (EditText) v.findViewById(R.id.edit_text_crime_title);
+        editTextTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                crime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        return v;
+    }
+}
